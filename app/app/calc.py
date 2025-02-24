45c12365212081b2c0de28cbe5004455cc065f44
datngovan <datngo.qlct@gmail.com>
2025-02-24 21:44:57 +0700

    add swagger

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,8 +2,8 @@
 Calculator functions
 """
 def add(x, y):
-    """Add x and y and return result."""
-    return x + y
+  """Add x and y and return result."""
+  return x + y
 def substract(x,y):
-    """Substract x and y and return result."""
-    return x - y
+  """Substract x and y and return result."""
+  return x - y
